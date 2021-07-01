226897ec615484b66f355087389d0833df1aca0d
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-02 01:54:28 +0530

    Calculate length of line

--- LineCompersion/src/Line_Compersion/LineCompersionProblem.java
+++ LineCompersion/src/Line_Compersion/LineCompersionProblem.java
@@ -1,8 +1,23 @@
 package Line_Compersion;
 
+import java.util.Scanner;
+
 public class LineCompersionProblem {
 	
-	public static void main(String[] args) {
-		System.out.println("Welcome Line Compersion Problem");
-	}
+	public static void main(String[] args){
+		// get value from user
+	   	Scanner myObj = new Scanner(System.in);
+	      	System.out.println(" Welcome to Line Compersion Computation ");
+	      	System.out.println("Enter x1, y1, x2, y2:  ");
+	        int coordinate_x1 = myObj.nextInt();
+	        int coordinate_y1 = myObj.nextInt();
+	        int coordinate_x2 = myObj.nextInt();
+	        int coordinate_y2 = myObj.nextInt();
+	        System.out.println("enter coordinate_x1: "+ coordinate_x1);
+	        System.out.println("enter coordinate_y1: "+ coordinate_y1);
+	        System.out.println("enter coordinate_x2: "+ coordinate_x2);
+	        System.out.println("enter coordinate_y2: "+ coordinate_y2);
+	        int Length =(int) (Math.sqrt((coordinate_x2-coordinate_x1)^2+(coordinate_y2-coordinate_y1)^2));
+	        System.out.println("Length of a line: "+Length);
+		}
 }
